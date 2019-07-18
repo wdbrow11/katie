f187560969bc8047e2f8ba526108996d8479cf91
Will Brown <williambrown@williams-air.lan>
2019-07-17 23:55:06 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var shoutouts = [
+  "You are really nice and positive and I love that you love Taylor Swift.",
+  "You make every morning a lot more fun and positive!",
+  "the college advice you gave me! you are super hardworking",
+  "your perseverance with any project and any difficulties.",
+  "Your kindness and consideration",
+  "You always stay persistent through the challenges you face and I really admire that aspect of yours!",
+  "your smile and persistence even though you ran into so many problems with your site.",
+  "that you are really persistant",
+  "Your style and how nice you are!",
+  "Your positivity and kindness and that you are always smiling even when something is difficult. That ability is very special and remember never to lose it<3",
+  "you're really fun to talk to and work with!",
+  "how sweet, nice, and kind you where. You really made me feel welcome and I love talking with you :)",
+  "I like talking to you, you're a very nice person with a great personality and I also like the aesthetics in your websites.",
+  "Your enthusiasm about everything. You are always so excited about even the smallest things and it pumps everyone up along the way.",
+]
+
+for (x of shoutouts) {
+  $("#shoutout-container").append("<p>" + x + "</p> <br>");
+}
